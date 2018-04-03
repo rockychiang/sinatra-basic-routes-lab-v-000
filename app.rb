13a84020cca7aa0d1e8eef69ed76dc8c01054dbd
Rocky Chiang <rocky@chem.ucla.edu>
2018-04-03 03:24:07 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,21 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get "/" do 
+    "Hello, World!"
+  end
+  
+  get "/name" do 
+    "My name is Rocky"
+  end
+  
+  get "/hometown" do 
+    "My hometown is Philadelphia"
+  end
+  
+  get "/favorite-song" do
+    "My favorite song is currently 'One More Light' by Linkin Park"
+  end
+  
 end
